1a3789d1af6e0c5cde89549d13b22085d4de328b
Ariane Deschenes Foucher <ariane.df@me.com>
2016-05-02 19:06:47 -0400

    jQuery AJAX request with HTML response

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,16 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).on('ready page:load', function() {
+  $('#new_tweet').submit(function(event) {
+    event.preventDefault();
+    var tweet = $('tweet_message').serialize();
+
+    $.ajax({
+      url: '/tweets?' + tweet
+      type: 'GET'
+      dataType: 'html'
+    }).done(function(data) {
+      $('products').html(data);
+    });
+  });
+});
